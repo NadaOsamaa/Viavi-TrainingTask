3586871f3063a26e7b5f7b0028d71489da8df636
unknown <maryamnzlawy@gmail.com>
2023-08-22 21:37:04 +0300

    added write function

--- viavi_c++_task/main.cpp
+++ viavi_c++_task/main.cpp
@@ -2,11 +2,37 @@ using namespace std;
 #include <iostream>
 #include <fstream>
 #include <regex>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+#include <sstream>
+#include <vector>
 
 // This code is designed to read phone numbers from a file and validate them based on certain criteria (checks if the phone number is exactly 11 digits long)
+string generate_phone_number() {
+    string prefixes[] = {"012", "011", "010", "015"};
+    int randomPrefixIndex = rand() % 4;
+    string phoneNumber = prefixes[randomPrefixIndex];
+
+    for (int i = 0; i < 8; ++i) {
+        phoneNumber += to_string(rand() % 10);
+    }
+
+    return phoneNumber;
+}
 
 class PhoneNo {
 public:
+    void write_to_file(vector<string> phones)
+    {
+        ofstream file("phone_numbers.txt");
+
+        for(int i = 0; i < phones.size(); i++) {
+            file << phones[i] << endl;
+        }
+
+        //return file;
+    }
     string readFile(const string& filename) {
         ifstream file(filename);
         string content;
@@ -35,6 +61,14 @@ public:
 
 int main() {
     PhoneNo PhoneNumber;
+    //generate and write numbers to file
+    vector<string> phone_number;
+    srand(static_cast<unsigned>(time(nullptr)));
+    for(int i = 20; i > 0; i--) {
+        string phoneNumber = generate_phone_number();
+        phone_number.push_back(phoneNumber);
+    }
+    PhoneNumber.write_to_file(phone_number);
 
     // Read from a file
     string content = PhoneNumber.readFile("phone_numbers.txt");
